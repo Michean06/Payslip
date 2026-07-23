5bc248e54c7f73dabd20d764d39c89df85a5c0ec
Jhon Arl Lauron <celendronjhonarl28@gmail.com>
2026-07-23 13:53:38 +0800

    Use API-first Vercel routes for /api handlers

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,1 +1,12 @@
-module.exports = require('./server');
+const expressApp = require('../server');
+const serverless = require('serverless-http');
+
+const handler = serverless(expressApp);
+
+module.exports = async function catchAllApiHandler(req, res) {
+  if (req.url && !req.url.startsWith('/api')) {
+    req.url = `/api${req.url.startsWith('/') ? req.url : `/${req.url}`}`;
+  }
+
+  return handler(req, res);
+};
